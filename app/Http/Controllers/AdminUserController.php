058f841823e06595ed94b41f6ce46bb7e595569a
Jalade <ayodejialalade29@gmail.com>
2026-09-21 00:28:11 +0100

    fix(AdminUserController): include latest price in user lands relationship

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -78,7 +78,8 @@ class AdminUserController extends Controller
     {
         $user->load([
             'kycVerification',
-            'userLands.land:id,title,price_per_unit',
+            'userLands.land:id,title',
+            'userLands.land.latestPrice',
             'recentTransactions', 
         ]);
 
